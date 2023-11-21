574ae12769411aef357a1aecd32925eb0ac8a147
Joeri van Alphen <jralphen@kpnmail.nl>
2023-11-21 14:54:27 +0100

    Created pathfinding algorithm interface

---
+++ Game AI/Assets/Scripts/Tools/AI/Pathfinding/Algoritms/AStar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Joeri.Tools.AI.Pathfinding
+{
+    public interface IAlgorithm
+    {
+        public LinkedList<Vector2Int> GetPath(Vector2Int _start, Vector2Int _end);
+    }
+}
