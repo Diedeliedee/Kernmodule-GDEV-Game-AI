e23cf60713901eface618cc1c2614f1793ef6df0
Joeri van Alphen <jralphen@kpnmail.nl>
2024-01-28 04:30:35 +0100

    Made own player movement logic

---
+++ Game AI/Assets/Behavior Trees/Objects/Player/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using Joeri.Tools.Movement;
+using Joeri.Tools.Utilities;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Properties:")]
+    [SerializeField] private float moveSpeed = 3;
+    [SerializeField] private float moveGrip = 3;
+
+    [Header("Reference:")]
+    [SerializeField] private Transform m_camera;
+
+    private Accel.Flat m_flatAcceleration = new();
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var deltaTime = Time.deltaTime;
+        var vert = Input.GetAxisRaw("Vertical");
+        var hor = Input.GetAxisRaw("Horizontal");
+
+        Vector3 forwardDirection = Vector3.Scale(new Vector3(1, 0, 1), m_camera.transform.forward);
+        Vector3 rightDirection = Vector3.Cross(Vector3.up, forwardDirection.normalized);
+
+        var moveDirection = forwardDirection.normalized * vert + rightDirection.normalized * hor;
+        transform.position += Vectors.FlatToVector(m_flatAcceleration.CalculateVelocity(Vectors.VectorToFlat(moveDirection.normalized) * moveSpeed, moveGrip, deltaTime) * deltaTime);
+    }
+}
---
+++ Game AI/Assets/Behavior Trees/Objects/Player/PlayerRotation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    [Header("Properties:")]
+    [SerializeField] private float m_sensitivity = 3f;
+    [SerializeField] private float m_pitchRange = 85f;
+
+    [Header("References:")]
+    [SerializeField] private Transform m_camera;
+    [SerializeField] private Transform m_pivot;
+
+    private float m_pitch;
+    private float m_yaw;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        m_pitch = m_camera.localEulerAngles.x;
+        m_yaw = m_pivot.localEulerAngles.y;
+    }
+
+    private void Update()
+    {
+        var iteration = new Vector2 (Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * m_sensitivity;
+
+        m_pitch = Mathf.Clamp(m_pitch - iteration.y, -m_pitchRange, m_pitchRange);
+        m_yaw = Mathf.Repeat(m_yaw + iteration.x, 360f);
+
+        m_camera.localEulerAngles = new Vector3(m_pitch, 0f, 0f);
+        m_pivot.localEulerAngles = new Vector3(0f, m_yaw, 0f);
+    }
+}
