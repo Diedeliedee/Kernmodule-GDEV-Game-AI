aa51c899f93a702350eee48ae8e460eb251af6c3
Joeri van Alphen <jralphen@kpnmail.nl>
2024-01-29 01:03:55 +0100

    Revert "Altered selector abort system"
    This reverts commit 183395bd212e255bc0b690c7d8848e6378a6c950.

--- Game AI/Assets/Scripts/Tools/AI/Behavior Tree/Nodes/Composites/Selector.cs
+++ Game AI/Assets/Scripts/Tools/AI/Behavior Tree/Nodes/Composites/Selector.cs
@@ -5,36 +5,43 @@ namespace Joeri.Tools.AI.BehaviorTree
     public class Selector : CompositeNode
     {
         private int m_index = 0;
+        private int m_lastIndex = 0;
 
         public Selector(params Node[] _children) : base(_children) { }
 
         public override State Evaluate()
         {
             //  Check node states of the children.
-            for (m_index = 0; m_index < children.Length; m_index++)
+            for (; m_index < children.Length; m_index++)
             {
                 switch (children[m_index].Evaluate())
                 {
                     //  If the current node has failed, move on to evaluate the next.
-                    case State.Failure:
-                        children[m_index].OnAbort();
-                        continue;
+                    case State.Failure: continue;
 
                     // If the current child is running, the selector is still running too.
                     case State.Running: return State.Running;
 
                     //  If the current child has been a succes, the selector has been a succes too.
-                    case State.Succes: return State.Succes;
+                    case State.Succes:
+                        if (m_index < m_lastIndex) children[m_lastIndex].OnAbort();
+                        m_lastIndex = m_index;
+                        return State.Succes;
                 }
             }
 
             //  If no children are running, none can be selected and the selector has failed.
+            m_index = 0;
+            m_lastIndex = 0;
             return State.Failure;
         }
 
         public override void OnAbort()
         {
             children[m_index].OnAbort();
+
+            m_index = 0;
+            m_lastIndex = 0;
         }
 
         public override void OnDraw(Vector3 _center)
